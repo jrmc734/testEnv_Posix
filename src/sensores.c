42e17c928506ba2d68f37aceb704866658f7557a
Juliane <juliane.coutinho734@gmail.com>
2025-03-10 14:19:50 -0300

    Files: including sensores.c file

---
+++ src/sensores.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+
+int main()
+{
+    return 0;
+}
