e1dc59ff190f96716b43c9f5ca0681ae227bbf69
Juliane <juliane.coutinho734@gmail.com>
2025-03-10 14:18:55 -0300

    Files: including controlador.c file

---
+++ src/controlador.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <mqueue.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+int main()
+{
+    return 0;
+}
