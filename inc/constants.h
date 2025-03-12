9af2854415a76b08206cedd005eef967c26af8b3
Rafael Bernardo <rafaelbnneto@gmail.com>
2025-03-12 19:08:20 -0300

    feat(mqueue): create message queue and write to it
    - add command to create message queue in the controller
    - open and write to the message queue continuouly in instrument_cluster.c
    - read mq and print buffer in the controller

--- inc/constants.h
+++ inc/constants.h
@@ -1,5 +1,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANS_H
+#define QUEUE_PERMISSIONS 0660
+#define MQ_MAX_MESSAGES 10
+#define MQ_MAX_MSG_SIZE 128
 
 #define SHM_NAME "/my_shm"
 #define SEM_NAME "/my_sem"
