ebaf144ded70ef6aa2fb6ea085521a3a8cbf421e
Juliane <juliane.coutinho734@gmail.com>
2025-03-14 08:06:02 -0300

    "Adding temperature data in the sensor_simulator

--- src/sensor_simulator.c
+++ src/sensor_simulator.c
@@ -16,6 +16,9 @@ int speed_increasing = 1;
 int cur_rpm = 1000;
 int rpm_increasing = 1;
 
+int cur_temp = 75;
+int temp_increasing = 1;
+
 void thread_sleep()
 {
     usleep(100000 + rand() % 500000);
@@ -43,7 +46,7 @@ void *speed_thread(void *args)
                 speed_increasing = 1;
         }
         sprintf(shm, "S: %d", cur_speed);
-        printf("<%s>\n", shm);
+        printf("Message sent to shared memory: <%s>\n", shm);
         sem_post(sem);
         thread_sleep();
     }
@@ -77,6 +80,34 @@ void *rpm_thread(void *args)
     }
 }
 
+void *temperature_thread(void *args)
+{
+    shm_args *arguments = (shm_args *)args;
+    char *shm = arguments->shm;
+    sem_t *sem = arguments->sem;
+
+    while (1)
+    {
+        sem_wait(sem);
+        if (temp_increasing)
+        {
+            cur_temp += rand() % 10;
+            if (cur_temp >= 105)
+                temp_increasing = 0;
+        }
+        else
+        {
+            cur_temp -= rand() % 10;
+            if (cur_temp <= 75)
+                temp_increasing = 1;
+        }
+        sprintf(shm, "T: %d", cur_temp);
+        printf("Message sent to shared memory: <%s>\n", shm);
+        sem_post(sem);
+        thread_sleep();
+    }
+}
+
 int main()
 {
     int shm_fd = open_shm(SHM_NAME);
@@ -85,6 +116,7 @@ int main()
 
     pthread_t thread1_id;
     pthread_t thread2_id;
+    pthread_t thread3_id;
     
     shm_args thread_args;
     thread_args.shm = shm_ptr;
@@ -92,10 +124,12 @@ int main()
 
     pthread_create(&thread1_id, NULL, &speed_thread, &thread_args);
     pthread_create(&thread2_id, NULL, &rpm_thread, &thread_args);
-    
+    pthread_create(&thread3_id, NULL, &temperature_thread, &thread_args);
+
 
     pthread_join(thread1_id, NULL);
     pthread_join(thread2_id, NULL);
+    pthread_join(thread3_id, NULL);
 
     munmap(shm_ptr, 4096);
     close(shm_fd);
