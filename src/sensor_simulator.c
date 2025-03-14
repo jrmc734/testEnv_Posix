0307186383eb7a020cb88dd00704f15a2e21b9e2
Juliane <juliane.coutinho734@gmail.com>
2025-03-14 08:00:04 -0300

    Adding speed data to sensor_simulator

--- src/sensor_simulator.c
+++ src/sensor_simulator.c
@@ -10,13 +10,44 @@
 #include "shm_utils.h"
 #include "constants.h"
 
+int cur_speed = 0;
+int speed_increasing = 1;
+
+int cur_rpm = 1000;
+int rpm_increasing = 1;
+
 void thread_sleep()
 {
     usleep(100000 + rand() % 500000);
 }
 
-int cur_rpm = 1000;
-int rpm_increasing = 1;
+void *speed_thread(void *args)
+{
+    shm_args *arguments = (shm_args *)args;
+    char *shm = arguments->shm;
+    sem_t *sem = arguments->sem;
+
+    while (1)
+    {
+        sem_wait(sem);
+        if (speed_increasing)
+        {
+            cur_speed += rand() % 10;
+            if (cur_speed >= 190)
+                speed_increasing = 0;
+        }
+        else
+        {
+            cur_speed -= rand() % 10;
+            if (cur_speed <= 10)
+                speed_increasing = 1;
+        }
+        sprintf(shm, "S: %d", cur_speed);
+        printf("<%s>\n", shm);
+        sem_post(sem);
+        thread_sleep();
+    }
+}
 
 void *rpm_thread(void *args)
 {
@@ -53,14 +84,18 @@ int main()
     char *shm_ptr = map_shm(shm_fd, PROT_WRITE);
 
     pthread_t thread1_id;
+    pthread_t thread2_id;
     
     shm_args thread_args;
     thread_args.shm = shm_ptr;
     thread_args.sem = sem;
 
-    pthread_create(&thread1_id, NULL, &rpm_thread, &thread_args);
+    pthread_create(&thread1_id, NULL, &speed_thread, &thread_args);
+    pthread_create(&thread2_id, NULL, &rpm_thread, &thread_args);
+    
 
     pthread_join(thread1_id, NULL);
+    pthread_join(thread2_id, NULL);
 
     munmap(shm_ptr, 4096);
     close(shm_fd);
